96aa3cfd2d9743c3421f764193353e800353daf8
josueCarvajal <josue.crs97@gmail.com>
2019-02-02 10:38:17 -0600

    created the node class and respective pointers

--- src/LinkedList/Node.java
+++ src/LinkedList/Node.java
@@ -1,5 +1,28 @@
 package LinkedList;
 
 public class Node {
+	private Node next;
+	private int data;
+	
+	public Node(int data, Node next) {
+		this.next = next;
+		this.data = data;
+	}
+	public Node getNext() {
+		return next;
+	}
 
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	
 }
